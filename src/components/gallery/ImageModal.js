d26b165bd48d78be47c86a2eb52f03ea95911d0f
Miklós Kondász <miklos.kondasz@gmail.com>
2025-04-06 20:40:56 +0200

    Configure ImageModal

--- src/components/gallery/ImageModal.js
+++ src/components/gallery/ImageModal.js
@@ -9,12 +9,12 @@ const ImageModal = ({ isOpen, images, startIndex, onClose }) => {
     const formattedImages = images.map(image => ({
         original: image.url,
         thumbnail: image.url,
-        description: image.caption || "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        description: image.caption || ""
     }));
 
     return (
-        <div className="fixed inset-0 bg-black/90 z-50 flex items-center justify-center p-4">
-            <div className="relative max-w-full max-h-full w-full h-full">
+        <div className="fixed inset-0 bg-gray-900 bg-opacity-75 z-50 flex items-center justify-center p-4">
+            <div className="relative w-full h-full flex items-center justify-center">
                 <button
                     className="absolute top-4 right-4 z-50 text-white hover:text-gray-300 transition-colors"
                     onClick={onClose}
@@ -25,28 +25,32 @@ const ImageModal = ({ isOpen, images, startIndex, onClose }) => {
                     </svg>
                 </button>
 
-                <ImageGallery
-                    items={formattedImages}
-                    startIndex={startIndex}
-                    showPlayButton={false}
-                    showFullscreenButton={false}
-                    showThumbnails={false}
-                    additionalClass="image-gallery-modal"
-                    renderItem={(item) => (
-                        <div className="flex flex-col items-center">
-                            <img
-                                className="image-gallery-image"
-                                src={item.original}
-                                alt={item.description}
-                            />
-                            {item.description && (
-                                <div className="image-gallery-description text-white mt-4 max-w-2xl text-center text-sm px-4">
-                                    {item.description}
-                                </div>
-                            )}
-                        </div>
-                    )}
-                />
+                <div className="max-w-[90vw] max-h-[90vh] w-full">
+                    <ImageGallery
+                        items={formattedImages}
+                        startIndex={startIndex}
+                        showPlayButton={false}
+                        showFullscreenButton={false}
+                        showThumbnails={false}
+                        additionalClass="image-gallery-modal"
+                        slideDuration={0}  // Disables slide animation
+                        slideInterval={0}  // Disables auto-slide
+                        renderItem={(item) => (
+                            <div className="flex flex-col items-center justify-center h-full">
+                                <img
+                                    className="image-gallery-image max-h-[70vh] object-contain"
+                                    src={item.original}
+                                    alt={item.description}
+                                />
+                                {item.description && (
+                                    <div className="image-gallery-description text-white mt-4 max-w-2xl text-center text-sm px-4">
+                                        {item.description}
+                                    </div>
+                                )}
+                            </div>
+                        )}
+                    />
+                </div>
             </div>
         </div>
     );
